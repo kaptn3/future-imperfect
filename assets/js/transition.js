4ccc6d6aca592de401b7b10a54346a81ae35d5e5
ViKapitoshka <kapitonenko.vick@ya.ru>
2017-06-09 13:51:20 +0300

    add script transition

---
+++ assets/js/transition.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("a[href*=#]").on("click", function(e){
+		if ($("#header").height()>50) {
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top - 65
+			}, 1000);
+			e.preventDefault();
+			return false;
+		}
+	});
+});
